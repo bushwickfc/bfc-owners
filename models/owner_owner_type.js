cb7c0c7c9e32c3ff5e1a3fa6a3238efcccb77274
Kevin Qiu <kevin@arena.io>
2018-08-30 14:51:24 -0400

    Add autoincrement

--- models/owner_owner_type.js
+++ models/owner_owner_type.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   var Model = sequelize.define('owner_owner_type', {
     'id': {
       type: DataTypes.INTEGER,
+      autoIncrement: true,
       primaryKey: true
     },
     'email': {
