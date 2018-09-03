d7434cfdb3734989da04c45d14211446e503720a
Kevin Qiu <kevin@arena.io>
2018-09-03 16:36:01 -0400

    hour log id

--- models/hour_log.js
+++ models/hour_log.js
@@ -2,20 +2,22 @@
 
 module.exports = (sequelize, DataTypes) => {
   var Model = sequelize.define('hour_log', {
+    'id': {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
     'email': {
       type: DataTypes.STRING,
-      primaryKey: true
     },
     'amount': {
       type: DataTypes.INTEGER,
     },
     'hour_reason': {
       type: DataTypes.STRING,
-      primaryKey: true
     },
     'hour_date': {
       type: DataTypes.DATE,
-      primaryKey: true
     },
     'created_at': {
       type: DataTypes.DATE,
